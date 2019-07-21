16988f137e6fb1da24c9941980bdcd00b4795060
Jan Vincent Liwanag <jvliwanag@gmail.com>
2019-07-21 17:09:06 +0800

    Flatten checks

--- src/Scraper.ts
+++ src/Scraper.ts
@@ -171,6 +171,20 @@ function typeToPropSpec(typ: Type<ts.Type>, reference?: Symbol, name?: String): 
     typ = typ.getNonNullableType();
   }
 
+  // Symbol
+  let sym = typ.getSymbol();
+
+  // Node
+  let decl: Node | undefined;
+  let knownPropType: PropType | undefined;
+  if (typeof sym !== "undefined") {
+    decl = sym.getDeclarations()[0];
+
+    if (typeof decl !== "undefined") {
+      knownPropType = getKnownPropTypeFromNode(decl);
+    }
+  }
+
   let propType: PropType;
 
   if (isTypeVoid(typ)) {
@@ -181,7 +195,6 @@ function typeToPropSpec(typ: Type<ts.Type>, reference?: Symbol, name?: String): 
     propType = numberPropType;
   } else if (typ.isBoolean()) {
     propType = booleanPropType
-
   } else if (typ.compilerType.isLiteral()) {
     // Does not inclue boolean -- see https://github.com/Microsoft/TypeScript/issues/26075
     let value = typ.compilerType.value;
@@ -189,52 +202,45 @@ function typeToPropSpec(typ: Type<ts.Type>, reference?: Symbol, name?: String): 
   } else if (typ.isBooleanLiteral()) {
     // TODO look for a better way for this
     propType = literalPropType(typ.getText() === "true");
+  } else if (typeof knownPropType !== "undefined") {
+    propType = knownPropType;
+  } else if (typeof decl !== "undefined" && TypeGuards.isSignaturedDeclaration(decl)) {
+    let paramPropSpec = decl.getParameters().map((p) => typeToPropSpec(p.getType(), reference, p.getName()));
+    let returnPropSpec = typeToPropSpec(decl.getReturnType(), reference);
+
+    propType = fnPropType(paramPropSpec, returnPropSpec);
+  } else if (typ.isObject()) {
+    // Check of object goes here since fucntions are detected as objects as well
+    propType = objectPropType(typ.getProperties().map((p) => symbolToNamedPropSpec(p, reference)));
+  } else if (typ.isObject()) {
+    // TODO Remove this duplication later on
+    propType = objectPropType(typ.getProperties().map((p) => symbolToNamedPropSpec(p, reference)));
   } else if (typ.isUnion()) {
-    let options: PropType[] = typ.getUnionTypes().map((t) => typeToPropSpec(t, reference).propType);
+    try {
+      let options: PropType[] = typ.getUnionTypes().map((t) => typeToPropSpec(t, reference).propType);
 
-    propType = unionPropType(options);
+      propType = unionPropType(options);
+    } catch (e) {
+      throw "Unable to form union for " + n + ": " + e;
+    }
   } else {
-    // Symbol
-    let sym = typ.getSymbol();
+    let symText, declText;
 
-    // Node
-    let decl: Node | undefined;
-    if (sym) {
-      decl = sym.getDeclarations()[0];
+    if (typeof sym !== 'undefined') {
+      symText = `(Symbol: ${sym.getName()})`;
+    } else {
+      symText = "(No Symbol)";
     }
 
     if (typeof decl !== "undefined") {
-      let knownPropType = getKnownPropTypeFromNode(decl);
-
-      if (typeof knownPropType !== "undefined") {
-        propType = knownPropType;
-      } else if (TypeGuards.isSignaturedDeclaration(decl)) {
-        let paramPropSpec = decl.getParameters().map((p) => typeToPropSpec(p.getType(), reference, p.getName()));
-        let returnPropSpec = typeToPropSpec(decl.getReturnType(), reference);
-
-        propType = fnPropType(paramPropSpec, returnPropSpec);
-      } else if (typ.isObject()) {
-        // Check of object goes here since fucntions are detected as objects as well
-        propType = objectPropType(typ.getProperties().map((p) => symbolToNamedPropSpec(p, reference)));
-      } else {
-        throw `Unknown propType for ${n}: ${typ.getText()} ` +
-        `(Kind: ${decl.getKindName()}) ` +
+      declText = `(Kind: ${decl.getKindName()}) ` +
         `(Source: ${decl.getSourceFile().getFilePath()} ` +
         `Line: ${decl.getStartLineNumber()} ` +
-        `Col: ${decl.getStartLinePos()})`
-      }
-    } else if (typ.isObject()) {
-      // TODO Remove this duplication later on
-      propType = objectPropType(typ.getProperties().map((p) => symbolToNamedPropSpec(p, reference)));
+        `Col: ${decl.getStartLinePos()})`;
     } else {
-      let symText;
-      if (typeof sym !== 'undefined') {
-        symText = `Symbol: sym.getName()`;
-      } else {
-        symText = "No Symbol";
-      }
-      throw `Unknown propType for ${n}: ${typ.getText()} (${symText}) (Missing decl)`;
+      declText = "(No decl)"
     }
+    throw `Unknown propType for ${n}: ${typ.getText()} ${symText} ${declText}`;
   }
 
   return { propType, isNullable };
